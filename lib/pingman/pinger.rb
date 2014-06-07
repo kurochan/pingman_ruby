f5b16847b5bb21bf776fb8531698b800bb658887
kurochan <kuro@kurochan.org>
2014-06-07 19:39:04 +0900

    localhostへpingしてrubyの処理の遅延時間を考慮するように

--- lib/pingman/pinger.rb
+++ lib/pingman/pinger.rb
@@ -1,15 +1,24 @@
 require 'net/ping'
 
 class Pinger
+  def initialize
+    pe = Net::Ping::External.new('localhost')
+    pe.timeout = 2
+    pe.ping
+    pe.ping
+    start = Time.now
+    pe.ping
+    @offset = Time.now - start
+  end
+
   def ping(line)
     return unless line.hostname || line.address
     line.log = line.log.slice(0, 14)
-# Resolv::DNS.new 
-    pe = Net::Ping::External.new(line.address || line.hostname)
+    pe = Net::Ping::External.new line.address
     pe.timeout = 2
     start = Time.now
     line.res = pe.ping
-    line.rtt = Time.now - start
+    line.rtt = Time.now - start - @offset
     line.loss = (line.snt * line.loss + (line.res ? 0 : 1)) / (line.snt + 1).to_f
     line.avg = (line.snt * line.avg + line.rtt) / (line.snt + 1).to_f if line.res
     line.snt += 1
